21ba1dba47eedda644411b624715692de6ce578d
amanullahshah32 <nafi17211@gmail.com>
2024-09-18 23:12:56 +0600

    new change

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,17 +10,15 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css('article.product_pod')
 
         for book in books:
-            yield
-            {
-                'name' : book.css('h3 a::text').get(),
-                'price' : book.css(".product_price .price_color::text").get(),
-                'url' : book.css("h3 a").attrib['href'],
-            }
-        next_page = response.css('li.next a ::attr(href)').get()   
-        if next_page is not None:
+            
+            relative_url = book.css('h3 a ::attr(href)').get()   
+            
             if 'catalogue/' in next_page:
-                next_page_url= 'https://books.toscrape.com/' + next_page
+                book_url= 'https://books.toscrape.com/' + relative_url
             else:
-                next_page_url= 'https://books.toscrape.com/catalogue/' + next_page
+                book_url= 'https://books.toscrape.com/catalogue/' + relative_url
 
-            yield response.follow(next_page_url, callback= self.parse)
+            yield response.follow(book_url, callback= self.parse_book_page)
+    
+    def parse_book_page(self, response):
+        pass
